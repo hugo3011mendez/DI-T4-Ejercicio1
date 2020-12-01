07819d783b84ef9ff289911ff9d3394a4db35ba2
Hugo Mendez <hugo3011mendez@gmail.com>
2020-12-01 11:37:39 +0100

    Agregar archivos de proyecto.

---
+++ Ejercicio1/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Ejercicio1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnIzquierdo = new System.Windows.Forms.Button();
+            this.btnDerecho = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnIzquierdo
+            // 
+            this.btnIzquierdo.Location = new System.Drawing.Point(115, 108);
+            this.btnIzquierdo.Name = "btnIzquierdo";
+            this.btnIzquierdo.Size = new System.Drawing.Size(94, 47);
+            this.btnIzquierdo.TabIndex = 0;
+            this.btnIzquierdo.Text = "Izquierdo";
+            this.btnIzquierdo.UseVisualStyleBackColor = true;
+            this.btnIzquierdo.MouseMove += new System.Windows.Forms.MouseEventHandler(this.btnIzquierdo_MouseMove);
+            // 
+            // btnDerecho
+            // 
+            this.btnDerecho.Location = new System.Drawing.Point(509, 108);
+            this.btnDerecho.Name = "btnDerecho";
+            this.btnDerecho.Size = new System.Drawing.Size(92, 48);
+            this.btnDerecho.TabIndex = 1;
+            this.btnDerecho.Text = "Derecho";
+            this.btnDerecho.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDerecho);
+            this.Controls.Add(this.btnIzquierdo);
+            this.Name = "Form1";
+            this.Text = "Mouse Tester";
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnIzquierdo;
+        private System.Windows.Forms.Button btnDerecho;
+    }
+}
+
---
+++ Ejercicio1/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Mouse Move del Form
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Text = Form1.MousePosition.ToString();
+        }
+
+        // Mouse Move del botón izquierdo
+        private void btnIzquierdo_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+    }
+}
